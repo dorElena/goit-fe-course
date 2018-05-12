41025f5235849a084002b2c1c8068df1218fc131
Elena Hurtenko <elena.hurtenko@gmail.com>
2018-05-12 19:41:42 +0300

    js-mod-04

--- js/module-04/js/index.js
+++ js/module-04/js/index.js
@@ -11,65 +11,47 @@ const products = {
 };
 
 function Cashier(name, products) {
+  
   this.name = name;
   this.products = products;
-  //totalPrice - число, общая стоимость покупок текущего покупателя, всегда начинается с 0 
   this.totalPrice = 0;
-  //customerMoney - число, сумма введенная пользователем в prompt при запросе денег, всегда начинается с 0
   this.customerMoney = 0;
-  //changeAmount - число, сдача, всегда начинается с 0
   this.changeAmount = 0;
-  //countTotalPrice(order) - метод, получает список покупок,
-  // считает общую сумму исходя из поля products
   this.countTotalPrice = function(order) {
     for (let key in order) {
-      if (products.hasOwnProperty(key) === order.hasOwnProperty(key)) {
+      if (products.hasOwnProperty(key)) {
         this.totalPrice += (products[key] * order[key]);
       }
     }
-    return this.totalPrice;
   };
-   /*getCustomerMoney() - метод, при вызове показывает prompt, в котором указана общая сумма покупок
-   из поля totalPrice. Ожидает ввода пользователя и записывает результат ввода в поле customerMoney.
-    Проверить что пользователь ввел число. Также customerMoney должна быть не меньше чем totalPrice. 
-    Просит покупателя ввести сумму до тех пор, пока не будет введена корректная сумма - число, 
-    равное или больше чем totalPrice. Если пользователь нажмет Cancel, то функция возвращает null.*/
+  
   this.getCustomerMoney = function() {
     this.customerMoney = prompt(
       `Общая сумма покупок ${this.totalPrice}. Введите сумму не менее ${this.totalPrice}.`
     );
     if (this.customerMoney === null) {
-      return null;
+      break;
     }
-    if (!Number.isNaN(Number(this.customerMoney)) && this.customerMoney < this.totalPrice) {
-      this.getCustomerMoney();
-    }
-    return this.customerMoney;
   };
-  //countChange() - метод, считает сдачу, разницу между общей суммой покупок и деньгами покупателя.
+  
   this.countChange = function() {
     this.changeAmount = this.customerMoney - this.totalPrice;
-    return this.changeAmount;
   };
-  //reset() - метод, сбрасывает поля totalPrice, customerMoney и changeAmount в 0.
+  
   this.reset = function() {
     this.totalPrice = 0;
     this.customerMoney = 0;
     this.changeAmount = 0;
-    return this.totalPrice, this.customerMoney, this.changeAmount;
   };
-  /*serve(order) - метод, вызывается при обслуживании пользователя. Получает order - список покупок пользователя,
-    вызывает метод countTotalPrice для подсчета общей суммы, getCustomerMoney для запроса денег покупателя,
-    countChange для подсчета сдачи при успешном вводе пользователя. При успешном обслуживании возвращает
-     строку `Спасибо за покупку, ваша сдача ${сдача}`,
-      при неудачном 'Очень жаль, что-то пошло не так, приходите еще'.
-       Вызывает метод reset при любом исходе обслуживания.*/
+  
   this.serve = function(order) {
     this.countTotalPrice(order);
     this.getCustomerMoney();
-    this.countChange();
-    if (this.customerMoney >= this.totalPrice) {
+    if (Number.isNaN(Number(this.customerMoney)) && this.customerMoney >= this.totalPrice) {
+      
+      this.countChange();
       console.log(`Спасибо за покупку, ваша сдача ${this.changeAmount}`);
+    
     } else {
       console.log("Очень жаль, что-то пошло не так, приходите еще");
     }
@@ -82,7 +64,3 @@ const order = { bread: 2, milk: 2, apples: 1, cheese: 1 };
 const cashier = new Cashier("Mango", products);
 
 cashier.serve(order);
-/*
-    При вызове cashier.serve, countTotalPrice посчитает общую сумму равную 20+30+20+60, итого 130
-    Далее выполнение идет по вышеописанному алгоритму.
-  */
