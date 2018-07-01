71e1ba1f9870e18271250623da63847eb15b9cee
Elena Hurtenko <elena.hurtenko@gmail.com>
2018-07-01 11:16:06 +0300

    js-m-10

--- js/module-10/js/index.js
+++ js/module-10/js/index.js
@@ -1,15 +1,5 @@
 "use strict";
 
-/*
-  Написать приложение для работы с REST сервисом, 
-  все функции делают запрос и возвращают Promise 
-  с которым потом можно работать. 
-  
-  Документацию по бэкенду и пример использования прочитайте 
-  в документации https://github.com/trostinsky/users-api#users-api.
-  Сделать минимальный графический интерфейс в виде панели с полями и кнопками. 
-  А так же панелью для вывода результатов операций с бэкендом.
-*/
 const apiUrl = "https://test-users-api.herokuapp.com/users/";
 
 const btnGetAllUsers = document.querySelector('.js-btn-get-all-users');
@@ -44,19 +34,16 @@ const inputUpdateUserName = document.querySelector('.js-input-update-user-name')
 
 btnUpdateUserNname.addEventListener('click', updateUser);
 
-//функция getAllUsers() - должна вернуть текущий список всех пользователей в БД.
 function getAllUsers(evt) {
   evt.preventDefault();
   fetchUsers(apiUrl).then(createUser);
 }
 
-//функция getUserById(id) - должна вернуть пользователя с переданным id.
 function getUserById(evt) {
   evt.preventDefault();
   fetchUsers(apiUrl).then(userById);
 }
 
-//функция addUser(name, age) - должна записывать в БД юзера с полями name и age.
 function addNewUser(evt) {
   evt.preventDefault();
   const newUserObj = {
@@ -67,13 +54,11 @@ function addNewUser(evt) {
   fetchUsersPost(newUserObj).then(newUser);
 }
 
-//функция removeUser(id) - должна удалять из БД юзера по указанному id.
 function removeUser(evt) {
   evt.preventDefault();
   fetchDelUsers(inputRremoveUser.value).then(delUser);
 }
 
-//функция updateUser(id, user) - должна обновлять данные пользователя по id. 
 function updateUser(evt) {
   evt.preventDefault();
   const updateUserObj = {
@@ -100,8 +85,7 @@ function userById(arrUsers) {
   if (userData === undefined) {
     resultGetUserById.textContent = "Пользователь не найден";
   }
-  resultGetUserById.textContent = 
-    `id: ${userData.id}, name: ${userData.name}, age: ${userData.age}`;
+  resultGetUserById.textContent = `id: ${userData.id}, name: ${userData.name}, age: ${userData.age}`;
 }
 
 function newUser(user) {
@@ -116,6 +100,7 @@ function delUser(user) {
   if (user.status === 200) {
     resultRemoveUser.textContent = `Юзер удален из БД`;
   }
+  resultRemoveUser.textContent = `Что-то пошло не так`;
 }
 
 function fetchUsers(url) {
